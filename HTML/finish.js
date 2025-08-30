eb8535765cf82c2d81b31d4a59987d4e4672bdd3
smaller011 <divinebolaji3@gmail.com>
2025-08-30 05:37:25 +0100

    Initial Commit

---
+++ HTML/finish.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+      console.log("Confetti rain starting...");
+
+       function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+       };
+      // run forever (continuous rain)
+      setInterval(() => {
+        confetti({
+          particleCount: 100,
+          startVelocity: 20,
+          spread: 360,
+          ticks: 60,
+          origin: {
+            x: Math.random(), // random horizontal position
+            y: 0 // always from the top
+          }
+        });
+      }, 300); // every 200ms
+    });
